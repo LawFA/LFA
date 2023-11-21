1706c78b964bca4d8fad8b00f381e62a2a5b52aa
SonJimin <lijnar100@naver.com>
2023-11-21 16:52:01 +0900

    Feat: 초기 세팅 환결설정 완료
    Spring Initializr 를 통한 스프링부트 환경 설정 완료
    Java11/Maven/Spring Boot 2.7.17/Dependencies추가

---
+++ src/main/java/com/lfa/lfa/LfaApplication.java
@@ -0,0 +1,13 @@
+package com.lfa.lfa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LfaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LfaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lfa/lfa/LfaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lfa.lfa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LfaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
